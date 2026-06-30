eb79a87bb004ce218dae588e22ad0ec5b3e934ab
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-06-30 22:57:29 +0530

    Wave 3

--- main.py
+++ main.py
@@ -799,7 +799,6 @@ async def send_direct_message(chat_id: int, text: str, reply_markup: dict = None
         payload["reply_markup"] = json.dumps(reply_markup)
     async with httpx.AsyncClient() as client:
         await client.post(url, json=payload)
-        print("Posted", payload)
 
 async def delete_telegram_message(chat_id: int, message_id: int):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteMessage"
